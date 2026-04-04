ff344b1953849dbf689d78ae86c33cb0b1751f72
Kalfask <kalfas1234@hotmail.com>
2026-04-04 18:53:41 +0300

    phase1 complete need testing

--- auth-service/src/main/java/com/x402/auth_service/AuthServiceApplication.java
+++ auth-service/src/main/java/com/x402/auth_service/AuthServiceApplication.java
@@ -2,8 +2,10 @@ package com.x402.auth_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.x402.auth_service", "com.x402.common"})
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
